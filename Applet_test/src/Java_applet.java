e56b28b30b6fc71a5e44b2961f4188cac9f3c117
himitu23 <himitu23centcircumflex@gmail.com>
2014-08-06 15:23:03 +0900

    init commit

---
+++ Applet_test/src/Java_applet.java
@@ -0,0 +1,27 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class Java_applet extends Applet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+	//	super.paint(arg0);
+		g.drawString("Output Strings",10,10);
+	}
+
+	
+	
+}
