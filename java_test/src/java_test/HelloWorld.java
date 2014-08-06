d572d8872e1c95f36cd444d0b958db85b817a968
himitu23 <himitu23centcircumflex@gmail.com>
2014-08-06 14:41:25 +0900

    change message to "SAUCEER"

--- java_test/src/java_test/HelloWorld.java
+++ java_test/src/java_test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {//‚±‚ê‚ªmainƒƒ\ƒbƒh
 		// TODO Auto-generated method stub
-System.out.println("HelloWorld!");
+		System.out.println("SAUCER!!");
 	}
 
 }
